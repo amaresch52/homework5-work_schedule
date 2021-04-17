aeec6eef09a06d112b5a01e49e115a5f526f81d8
Tom Hanks <amaresch@gmail.com>
2021-04-16 22:45:33 -0500

    adding boilerplate code

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$('#currentDay').text(dayjs().format('MMMM DD YYYY'));
+
+
+$("#hour-9 .description").val(localStorage.getItem("hour9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
