3e2ecc463d68b92978dffcb11dafb74acb155b9c
Tom Hanks <amaresch@gmail.com>
2021-04-17 02:12:02 -0500

    loads saved local storage on reload

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 $('#currentDay').text(dayjs().format('MMMM DD YYYY'));
 
-$('.saveBtn').on("click",function() {
+$('.saveBtn').on("click",function(event) {
     var timeSlot = $(this).siblings(".description").val();
-    var inputText = $(this).parent("#id");
-
+    var inputText = $(this).parent().attr("id");
+    event.preventDefault();
     console.log(this);
-localStorage.setItem(timeSlot,inputText);
+localStorage.setItem(inputText,timeSlot);
 });
 
-
-$("#hour-9 .description").val(localStorage.getItem("hour9"));
+function loadData() {
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
 $("#hour-12 .description").val(localStorage.getItem("hour-12"));
@@ -18,11 +18,36 @@ $("#hour-2 .description").val(localStorage.getItem("hour-2"));
 $("#hour-3 .description").val(localStorage.getItem("hour-3"));
 $("#hour-4 .description").val(localStorage.getItem("hour-4"));
 $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+}
+function timeSave() {
 
 var date = new Date();
 var currentHour = date.getHours();
 console.log(currentHour);
 
 $('.time-block').each(function() {
+    var inputHour = parseInt($(this).attr('id').split('hour-')[1]);
+    console.log(inputHour,currentHour)
+    if (inputHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    }
+    else if (inputHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+        
 
-})
+    
+    })
+}
+    timeSave();
+    loadData();
+
