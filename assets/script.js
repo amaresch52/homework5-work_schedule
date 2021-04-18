ae5b8e64208e2ed0bd6661b238cfa861a4c3bbbb
Tom Hanks <amaresch@gmail.com>
2021-04-17 19:01:07 -0500

    final code and clear schedule button added

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,46 @@
 $('#currentDay').text(dayjs().format('MMMM DD YYYY'));
 
-$('.saveBtn').on("click",function(event) {
+$('#delete').on("click", function() {
+    localStorage.clear();
+    location.reload();
+    return false;
+});
+
+//Save input onclick to localStorage
+$('.saveBtn').on("click",function() {
     var timeSlot = $(this).siblings(".description").val();
     var inputText = $(this).parent().attr("id");
-    event.preventDefault();
-    console.log(this);
+
 localStorage.setItem(inputText,timeSlot);
 });
 
+//Load data from local storage
 function loadData() {
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
 $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1 .description").val(localStorage.getItem("hour-1"));
-$("#hour-2 .description").val(localStorage.getItem("hour-2"));
-$("#hour-3 .description").val(localStorage.getItem("hour-3"));
-$("#hour-4 .description").val(localStorage.getItem("hour-4"));
-$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 }
+
 function timeSave() {
 
-var date = new Date();
-var currentHour = date.getHours();
-console.log(currentHour);
+var timeNow = dayjs().format('H');
 
+//Check time for css property
 $('.time-block').each(function() {
     var inputHour = parseInt($(this).attr('id').split('hour-')[1]);
-    console.log(inputHour,currentHour)
-    if (inputHour < currentHour) {
+    console.log(inputHour,timeNow)
+    if (inputHour < timeNow) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
     }
-    else if (inputHour === currentHour) {
+    else if (inputHour == timeNow) {
         $(this).removeClass("past");
         $(this).addClass("present");
         $(this).removeClass("future");
